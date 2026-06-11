bcd0314d95a3baa5ecbd5379bfe899423db5b80b
william-reges <william.reges1986@gmail.com>
2026-06-11 19:47:08 -0300

    refactor: implementando interceptor CorrelaitonId

--- src/main/java/com/example/payment/infraestructure/configuration/interceptor/CorrelationIdIHandlerInterceptor.java
+++ src/main/java/com/example/payment/infraestructure/configuration/interceptor/CorrelationIdIHandlerInterceptor.java
@@ -1,24 +1,32 @@
-package com.example.payment.infraestructure.interceptor;
+package com.example.payment.infraestructure.configuration.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import static java.util.Optional.ofNullable;
 
 @Component
 public class CorrelationIdIHandlerInterceptor implements HandlerInterceptor {
 
+    private final String CORRELATION_ID = "correlationId";
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         ofNullable(
-                request.getHeader("correlationID"))
+                request.getHeader(CORRELATION_ID))
                 .ifPresent(
-                        s -> MDC.put("correlationID", s));
+                        s -> MDC.put(CORRELATION_ID, s));
 
         return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        MDC.remove(CORRELATION_ID);
     }
 }
